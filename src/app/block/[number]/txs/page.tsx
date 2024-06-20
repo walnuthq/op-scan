4918d54bcb0737d88ac6e2ac7e2aa62174e0e267
adrianvrj <adrivargas48@gmail.com>
2024-06-20 14:07:56 -0600

    run prettier and lint

--- src/app/block/[number]/txs/page.tsx
+++ src/app/block/[number]/txs/page.tsx
@@ -1,8 +1,9 @@
 import BlckTxs from "@/components/pages/block/txs";
 
-const BlckTxsPage = ({ params: { number } }: { params: { number: bigint } }) => (
-    <BlckTxs number={number}/>
-  );
-  
-  export default BlckTxsPage;
-  
+const BlckTxsPage = ({
+  params: { number },
+}: {
+  params: { number: bigint };
+}) => <BlckTxs number={number} />;
+
+export default BlckTxsPage;
