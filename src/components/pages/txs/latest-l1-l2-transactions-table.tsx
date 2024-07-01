949d6a58e5ba87a68a1def3f90ff063cd6015bcd
Marco Araya Jiménez <75222804+coxmars@users.noreply.github.com>
2024-07-01 18:02:02 +0200

    feat: add additional improvements requested

--- src/components/pages/txs/latest-l1-l2-transactions-table.tsx
+++ src/components/pages/txs/latest-l1-l2-transactions-table.tsx
@@ -1,8 +1,8 @@
+// src/components/tx/latest-l1-l2-transactions-table.tsx
 import Link from "next/link";
 import { formatTimestamp } from "@/lib/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import { L1L2Transaction } from "@/lib/types";
-import { SquareArrowOutUpRight } from "lucide-react";
 import {
   Table,
   TableCaption,
@@ -12,6 +12,7 @@ import {
   TableBody,
   TableCell,
 } from "@/components/ui/table";
+import { SquareArrowOutUpRight } from "lucide-react";
 import { l1Chain } from "@/lib/chains";
 
 const LatestL1L2TransactionsTable = ({
@@ -22,46 +23,76 @@ const LatestL1L2TransactionsTable = ({
   <Card>
     <CardContent className="p-4">
       <Table className="w-full table-auto">
-        <TableCaption>
+        <TableCaption className="text-lg font-semibold">
           List of latest L1 - L2 transactions
         </TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="max-w-[10rem]">Block No</TableHead>
-            <TableHead className="max-w-[10rem]">L2 Tx Hash</TableHead>
-            <TableHead className="max-w-[10rem]">Timestamp</TableHead>
-            <TableHead className="max-w-[10rem]">L1 Tx Hash</TableHead>
-            <TableHead className="max-w-[10rem]">L1 Tx Origin</TableHead>
-            <TableHead className="max-w-[8rem]">Gas Limit</TableHead>
+            <TableHead className="px-4 py-2">Block No</TableHead>
+            <TableHead className="px-4 py-2">L2 Tx Hash</TableHead>
+            <TableHead className="px-4 py-2">Timestamp</TableHead>
+            <TableHead className="px-4 py-2">L1 Tx Hash</TableHead>
+            <TableHead className="px-4 py-2">L1 Tx Origin</TableHead>
+            <TableHead className="px-4 py-2">Gas Limit</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {transactions.map((transaction) => (
             <TableRow key={transaction.l2Hash}>
-              <TableCell className="max-w-[10rem] truncate">
-                <Link href={`https://explorer.l1.com/block/${transaction.l1BlockNumber}`}>
-                  {transaction.l1BlockNumber.toString()}
-                </Link>
+              <TableCell className="px-4 py-2 max-w-[10rem] truncate">
+                <div className="flex items-center gap-1 text-sm">
+                  <a
+                    className="flex items-center truncate text-primary hover:brightness-150"
+                    href={`${l1Chain.blockExplorers.default.url}/block/${transaction.l1BlockNumber}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="truncate">{transaction.l1BlockNumber.toString()}</span>
+                    <SquareArrowOutUpRight className="ml-1 w-4 h-4 flex-shrink-0" />
+                  </a>
+                </div>
               </TableCell>
-              <TableCell className="max-w-[10rem] truncate">
-                <Link href={`/txs/${transaction.l2Hash}`}>
-                  {transaction.l2Hash}
-                </Link>
+              <TableCell className="px-4 py-2 max-w-[14rem] truncate">
+                <div className="flex items-center gap-1 text-sm">
+                  <Link
+                    className="flex items-center truncate text-primary hover:brightness-150"
+                    href={`${l1Chain.blockExplorers.default.url}/tx/${transaction.l2Hash}`}
+                  >
+                    <span className="truncate">{transaction.l2Hash}</span>
+                    <SquareArrowOutUpRight className="ml-1 w-4 h-4 flex-shrink-0" />
+                  </Link>
+                </div>
               </TableCell>
-              <TableCell className="max-w-[10rem] truncate">
+              <TableCell className="px-4 py-2 max-w-[10rem] truncate">
                 {formatTimestamp(transaction.timestamp, false)}
               </TableCell>
-              <TableCell className="max-w-[10rem] truncate">
-                <Link href={`https://explorer.l1.com/tx/${transaction.l1TxHash}`}>
-                  {transaction.l1TxHash}
-                </Link>
+              <TableCell className="px-4 py-2 max-w-[14rem] truncate">
+                <div className="flex items-center gap-1 text-sm">
+                  <a
+                    className="flex items-center truncate text-primary hover:brightness-150"
+                    href={`${l1Chain.blockExplorers.default.url}/tx/${transaction.l1TxHash}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="truncate">{transaction.l1TxHash}</span>
+                    <SquareArrowOutUpRight className="ml-1 w-4 h-4 flex-shrink-0" />
+                  </a>
+                </div>
               </TableCell>
-              <TableCell className="max-w-[10rem] truncate">
-                <Link href={`https://explorer.l1.com/address/${transaction.l1TxOrigin}`}>
-                  {transaction.l1TxOrigin}
-                </Link>
+              <TableCell className="px-4 py-2 max-w-[14rem] truncate">
+                <div className="flex items-center gap-1 text-sm">
+                  <a
+                    className="flex items-center truncate text-primary hover:brightness-150"
+                    href={`${l1Chain.blockExplorers.default.url}/address/${transaction.l1TxOrigin}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="truncate">{transaction.l1TxOrigin}</span>
+                    <SquareArrowOutUpRight className="ml-1 w-4 h-4 flex-shrink-0" />
+                  </a>
+                </div>
               </TableCell>
-              <TableCell className="max-w-[8rem] truncate">
+              <TableCell className="px-4 py-2 max-w-[8rem] truncate">
                 {transaction.gasLimit}
               </TableCell>
             </TableRow>
